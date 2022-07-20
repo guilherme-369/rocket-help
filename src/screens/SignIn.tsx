ee95fcd566eb5ce87af825b7d6aa108666236244
guilherme soares <guilherme.dev36@gmail.com>
2022-07-20 14:41:09 -0300

    feat: add lib icons

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,33 @@
-import { VStack, Heading } from "native-base";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
 
-import Logo from '../assets/logo_primary.svg'
+import Logo from "../assets/logo_primary.svg";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
-export const SignIn = () => { 
+export const SignIn = () => {
+  const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-    <Logo/>
+      <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua Conta 
+        Acesse sua Conta
       </Heading>
+      <Input
+        placeholder="E-mail"
+        mb={5}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+      />
+      <Input
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        mb={5}
+        secureTextEntry
+      />
+      <Button title="Entrar" w="full" />
     </VStack>
   );
 };
