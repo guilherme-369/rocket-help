1afb7747650a50f3f15803454f23e1da74908974
guilherme soares <guilherme.dev36@gmail.com>
2022-07-20 20:10:22 -0300

    feat: create page SingIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 
@@ -6,28 +7,37 @@ import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export const SignIn = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   const { colors } = useTheme();
+
+  const handleSingIn = () => {
+    console.log(name, password);
+  };
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua Conta
+        Acesse sua conta
       </Heading>
       <Input
-        placeholder="E-mail"
+        placeholder="Name"
         mb={5}
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
       <Input
-        placeholder="Senha"
+        placeholder="Password"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         mb={5}
         secureTextEntry
+        onChangeText={setPassword}
       />
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSingIn} />
     </VStack>
   );
 };
